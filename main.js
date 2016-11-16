d519620ecef4a759e41d812ee3ab8e245553b4c9
jake <cakeandturtles@gmail.com>
2016-11-15 22:36:21 -0800

    updated index to update on the fly using pushState (not compatible with IE7 sadly.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var main_index, title, container, backButton, forwardButton;
+var main_index, title, container, backButton, forwardButton, firstButton, lastButton;
 main_index = max_index;
 
 function main() {
@@ -6,19 +6,26 @@ function main() {
     container = document.getElementById("comicbook");
     backButton = document.getElementById("back-button");
     forwardButton = document.getElementById("forward-button");
+    firstButton = document.getElementById("first-button");
+    lastButton = document.getElementById("last-button");
     
+    updateMainIndexFromUrlParams();
     updateDisplay(main_index);
 }
 window.onload = main;
 
+function updateMainIndexFromUrlParams() {
+    var params = window.location.search.substring(1);
+    var index = Number(params.split("&")[0].split("=")[1]);
+    if (isNaN(index)) return;
+    main_index = index;
+}
+
 function updateDisplay(index) {
     if (main_index != index) {
-        if (index != max_index) 
-            window.location = getPageString(index) + ".html";
-        else
-            window.location = "index.html";
-        return;
+        window.history.pushState('p'+index, comicTitles[index-1], 'index.html?p='+index);
     }
+    main_index = index;
     
     container.innerHTML = "";
     var page = document.createElement("img");
@@ -29,17 +36,25 @@ function updateDisplay(index) {
     if (index <= 1) {
         backButton.src = "leftarrow_grey.png";
         backButton.style.cursor = "default";
+        firstButton.src = "leftleftarrow_grey.png";
+        firstButton.style.cursor = "default";
     } else {
         backButton.src = "leftarrow.png";
         backButton.style.cursor = "pointer";
+        firstButton.src = "leftleftarrow.png";
+        firstButton.style.cursor = "pointer";
     }
     
     if (index >= max_index) {
         forwardButton.src = "rightarrow_grey.png";
         forwardButton.style.cursor = "default";
+        lastButton.src = "rightrightarrow_grey.png";
+        lastButton.style.cursor = "default";
     } else {
         forwardButton.src = "rightarrow.png";
         forwardButton.style.cursor = "pointer";
+        lastButton.src = "rightrightarrow.png";
+        lastButton.style.cursor = "pointer";
     }
 }
 function getPageString(index) {
@@ -53,12 +68,20 @@ function getPageString(index) {
 }
 function flipBack() {
     if (main_index <= 1) return;
-    
     updateDisplay(main_index-1);
 }
 
 function flipForward() {
     if (main_index >= max_index) return;
-    
     updateDisplay(main_index+1);
+}
+
+function flipFirst() {
+    if (main_index <= 1) return;
+    updateDisplay(1);
+}
+
+function flipLast() {
+    if (main_index >= max_index) return;
+    updateDisplay(max_index);
 }
